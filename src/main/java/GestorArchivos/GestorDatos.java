2249543c408d0030b4e2839e8a853739ec221938
DaVincci1417 <e.caniulaf01@ufromail.cl>
2023-04-27 07:57:12 -0400

    Second Commit

--- src/main/java/GestorArchivos/GestorDatos.java
+++ src/main/java/GestorArchivos/GestorDatos.java
@@ -4,29 +4,10 @@ import Model.Juego;
 import Model.Ronda;
 
 import java.io.*;
-import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.Files;
 import java.util.List;
 public class GestorDatos{
-    public Juego leerArchivoRondas(Juego juego, String direccionArchivo) {
-        String textoArchivo = "";
-        try {
-            File archivo = new File(direccionArchivo);
-            FileReader fr = new FileReader(archivo);
-            BufferedReader br = new BufferedReader(fr);
-
-            //Lee cada linea del archivo hasta que la linea sea nula
-            while ((textoArchivo = br.readLine()) != null) {
-                String[] data = textoArchivo.split(",");
-                juego.getRondas().add(new Ronda(data[0], data[1]));
-            }
-            br.close();
-            fr.close();
-        } catch (Exception e) {
-            System.out.println("Documento no disponible, favor contactar con administrador");
-        }
-        return juego;
-    }
 
     public boolean registrarRonda(Ronda ronda) {
         boolean lineaVacia = false;
